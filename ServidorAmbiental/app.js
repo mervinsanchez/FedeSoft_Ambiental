76ed561d47764fd59952fc8ac36695cdbc5d3e44
cjvillamil <cjvillamil.m@hotmail.com>
2018-08-01 21:44:46 -0500

    password

--- ServidorAmbiental/app.js
+++ ServidorAmbiental/app.js
@@ -3,13 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var mongoose=require('mongoose');
 
+var config=require('./config');
+var authenticate = require('./authenticate');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var puntoReciclaje=require('./routes/puntoReciclajeRouter');
-var mongoose=require('mongoose');
-var config=require('./config');
-var db=mongoose.connect('mongodb://localhost:27017/claseServidor');
+
+//var db=mongoose.connect('mongodb://localhost:27017/claseServidor');
+var db=mongoose.connect(config.mongoUrl);
 mongoose.connection.on('error',()=>{console.log("Base de datos en problemas")})
 mongoose.connection.once('open',()=>{console.log("Se ha conectado correctamente")})
 
@@ -24,6 +28,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- ServidorAmbiental/routes/users.js
+++ ServidorAmbiental/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var authenticate = require('../authenticate');
 var Usuario = require('../models/usuario');
 
 router.all((req, res, next) => {
@@ -34,5 +36,43 @@ router.post('/registro', (req, res, next) => {
         }
     })
 })
-
+router.post('/signup', (req, res, next) => {
+    Usuario.register(new Usuario({
+        username: req.body.username,
+        password: req.body.password,
+        nombre: req.body.nombre,
+        primerapellido: req.body.primerapellido,
+        segundoapellido: req.body.segundoapellido,
+        //correo: req.body.correo,
+        telcontacto: req.body.telcontacto
+    }), 
+      req.body.password, (err, Usuario) => {
+      if(err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({err: err});
+      }
+      else {
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful!'});
+        });
+      }
+    });
+  });
+  
+  router.post('/login', passport.authenticate('local'), (req, res,err) => {
+      console.log("entro");
+      
+      if(err){
+          console.log(err);
+      }
+      var token = authenticate.getToken({_id:req.user._id});
+      console.log(req.user);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true,token:token, status: 'You are successfully logged in!'});
+  });
+  
 module.exports = router;
